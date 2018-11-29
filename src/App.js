ce86689d15f9fbca6032f21a107fdc5cff39f0d0
Stefanie Chin <hi.stef.c@gmail.com>
2018-11-28 21:44:49 -0800

    feat: added Logo with tilt animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Nav from './components/navigation/Nav';
+import Logo from './components/logo/Logo';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Nav />
+        <Logo />
       </div>
     );
   }
