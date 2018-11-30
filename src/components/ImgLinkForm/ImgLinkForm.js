627a7d029e81fddc9e81920046a2fd8c1d4bcd9f
Stefanie Chin <hi.stef.c@gmail.com>
2018-11-30 15:50:14 -0800

    add particle.js to button

--- src/components/ImgLinkForm/ImgLinkForm.js
+++ src/components/ImgLinkForm/ImgLinkForm.js
@@ -1,5 +1,38 @@
 import React from 'react';
 import './ImgLinkForm.css';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 10
+    },
+    color: {
+      value: ['#d5d1b7']
+    },
+    shape: {
+      stroke: {
+        width: 8,
+        color: '#d5d1b7'
+      }
+    },
+    line_linked: {
+      opacity: 0,
+      color: '#fff',
+      width: 0.6,
+      enable_auto: false
+    }
+  },
+  interactivity: {
+    "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        }
+      }
+}
+}
 
 const ImgLinkForm = () => {
   return (
@@ -7,12 +40,16 @@ const ImgLinkForm = () => {
       <p className='f3 center'>
         {'Load a photo and watch the AI detect the faces'}
       </p>
-      <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' />
-          <button className='w-30 grow f4 link ph3 pv2 dib bg-dark'>Detect Face</button>
+          <input className='f4 pa2 w-70' type='text'/>
+          <button className='w-30 grow f4 link ph3 pv2 dib'>
+            <Particles
+              className='particles'
+              params={particlesOptions}
+            />
+            Detect Face
+          </button>
         </div>
-      </div>
     </div>
 
   )
