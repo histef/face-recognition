588ae593484cddbe118d81f5fc27d533bba7e50a
Stefanie Chin <hi.stef.c@gmail.com>
2018-12-12 22:23:54 -0800

    added dynamic name to Rank.js

--- client/src/components/Rank/Rank.js
+++ client/src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = ({ name, entries }) => {
   return (
     <div>
       <div className='f3 center' style ={{color: '#403326'}}>
-        {`<Name>, your current rank is...`}
+        {`${name}, your current rank is...`}
       </div>
       <div className='f1 center' style ={{color: '#403326'}}>
         {entries}
