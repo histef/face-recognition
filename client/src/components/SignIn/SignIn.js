4ffa44539e3f04bf3a5ed0222ae03bf8ac21b4d6
Stefanie Chin <hi.stef.c@gmail.com>
2018-12-17 22:22:55 -0800

    styling: change layout

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './SignIn.css';
 
 class SignIn extends Component{
   state = {
--- client/src/components/logo/Logo.js
+++ client/src/components/logo/Logo.js
@@ -1,13 +1,12 @@
 import React from 'react';
-
-import Tilt from 'react-tilt'
+import Tilt from 'react-tilt';
 
 import './Logo.css';
 import scLogo from'./SC-logo.PNG';
 
 const Logo = () => {
   return (
-    <div className='ma3 mt0'>
+    <div className='logo ma3 mt0'>
       <Tilt className="Tilt shadow-2" options={{ max : 25 }} style={{ height: 125, width: 125 }} >
         <div className="Tilt-inner">
           <img src={scLogo} alt='logo' />
