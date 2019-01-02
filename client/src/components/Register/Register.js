f23eb49674913a689f117094849d51b4707a31b2
Stefanie Chin <hi.stef.c@gmail.com>
2019-01-01 23:20:13 -0600

    change Register component to smart container

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,40 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Register = ({ onRouteChange }) => {
+class Register extends Component {
 
-return (
-  <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-  <main className="pa4 black-80">
-    <form className="measure">
-      <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-        <legend className="f2 fw6 ph0 mh0">Register</legend>
-        <div className="mt3">
-          <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-          <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
-        </div>
-        <div className="mt3">
-          <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-          <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
-        </div>
-        <div className="mv3">
-          <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-          <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
-        </div>
-      </fieldset>
-      <div className="">
-        <input
-          onClick={() => onRouteChange('homepage')}
-          className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
-      </div>
-      <div className="lh-copy mt3">
-        <p
-          onClick={() => onRouteChange('signin')}
-          className="f6 link dim black db pointer">Back to Sign In</p>
-      </div>
-    </form>
-  </main>
-  </article>
-  )
+  state = {
+    email: '',
+    password: '',
+    name: ''
+  }
+  
+  onNameChange = e =>{
+    this.setState({
+      signInName: e.target.value
+    })
+  }
+  
+  onEmailChange = e => {
+    this.setState({
+      signInEmail: e.target.value
+    })
+  }
+
+  onPasswordChange = e => {
+    this.setState({
+      signInPassword: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <main className="pa4 black-80">
+        <form className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f2 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+            </div>
+          </fieldset>
+          <div className="">
+            <input
+              onClick={() => onRouteChange('homepage')}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+          </div>
+          <div className="lh-copy mt3">
+            <p
+              onClick={() => onRouteChange('signin')}
+              className="f6 link dim black db pointer">Back to Sign In</p>
+          </div>
+        </form>
+      </main>
+      </article>
+    )
+  }
 }
 
 export default Register;
